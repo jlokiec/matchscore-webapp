dd079fc2465dcdb225ab0416f688759f9b89a9a6
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-29 22:15:06 +0200

    Create register form component

--- src/constants/Api.ts
+++ src/constants/Api.ts
@@ -1,3 +1,4 @@
 const BASE_URL = "http://localhost:8080/api";
 export const LEAGUE_CATEGORIES = BASE_URL + "/public/categories";
 export const LEAGUES = BASE_URL + "/public/leagues";
+export const REGISTER = BASE_URL + "/public/register";
