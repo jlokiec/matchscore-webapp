62a8500bd7d368bd06817b007efc6fb2c98b5051
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-30 20:31:20 +0200

    Apply store props and dispatch methods to LeagueBrowser

--- src/components/LeagueBrowser.tsx
+++ src/components/LeagueBrowser.tsx
@@ -1,47 +1,37 @@
 import React from 'react';
 import { League } from '../models/League';
-import axios from 'axios';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Alert from 'react-bootstrap/Alert';
 import Spinner from 'react-bootstrap/Spinner';
-import * as api from '../constants/Api';
+import { CombinedState } from '../reducers/rootReducer';
+import { connect } from 'react-redux';
+import { fetch } from '../actions/leagues';
+import { getLeaguesForCategoryId } from '../reducers/leaguesReducer';
+import { ThunkDispatch } from 'redux-thunk'
 
-interface LeagueBrowserProperties {
+interface CustomProps {
     categoryId: number
 }
 
-interface LeagueBrowserState {
+interface StateProps {
     loading: boolean,
-    error: boolean,
+    error?: object,
     leagues: Array<League>
 }
 
-export default class LeagueBrowser extends React.Component<LeagueBrowserProperties, LeagueBrowserState>{
-    constructor(props: LeagueBrowserProperties) {
-        super(props);
-        this.state = {
-            loading: false,
-            error: false,
-            leagues: []
-        };
-    }
+interface DispatchProps {
+    fetch: (categoryId: number) => void
+}
 
+type LeagueBrowserProperties = StateProps & CustomProps & DispatchProps;
+
+class LeagueBrowser extends React.Component<LeagueBrowserProperties, {}>{
     componentDidMount() {
-        this.setState({ loading: true });
-        axios.get(api.LEAGUES, {
-            params: {
-                category: this.props.categoryId
-            }
-        })
-            .then(response => this.setState({ loading: false, leagues: response.data }))
-            .catch(error => {
-                this.setState({ loading: false, error: true });
-                console.error(`Error while fetching league data: ${JSON.stringify(error)}`);
-            })
+        this.props.fetch(this.props.categoryId);
     }
 
     render() {
-        if (this.state.loading) {
+        if (this.props.loading) {
             return (
                 <div className="d-flex justify-content-center">
                     <Spinner animation="border" role="status">
@@ -50,7 +40,7 @@ export default class LeagueBrowser extends React.Component<LeagueBrowserProperti
                 </div>
             );
         }
-        if (!this.state.loading && this.state.error) {
+        if (!this.props.loading && this.props.error) {
             return (
                 <div className="d-flex justify-content-center">
                     <Alert key={1} variant="danger">Błąd pobierania danych</Alert>
@@ -59,8 +49,24 @@ export default class LeagueBrowser extends React.Component<LeagueBrowserProperti
         }
         return (
             <ListGroup variant="flush">
-                {this.state.leagues.map(league => <ListGroup.Item key={league.id}>{league.name}</ListGroup.Item>)}
+                {this.props.leagues.map(league => <ListGroup.Item key={league.id}>{league.name}</ListGroup.Item>)}
             </ListGroup>
         );
     }
 }
+
+const mapStateToProps = (states: CombinedState, customProps: CustomProps): StateProps => {
+    return {
+        loading: states.leagues.loading,
+        error: states.leagues.error,
+        leagues: getLeaguesForCategoryId(states.leagues, customProps.categoryId)
+    };
+}
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, any>, customProps: CustomProps): DispatchProps => {
+    return {
+        fetch: (categoryId: number) => dispatch(fetch(categoryId))
+    };
+}
+
+export default connect<StateProps, DispatchProps, CustomProps, CombinedState>(mapStateToProps, mapDispatchToProps)(LeagueBrowser);
