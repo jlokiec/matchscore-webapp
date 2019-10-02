63c81a002a46163910bceaaa906059ac81d0b2ba
Jakub Łokieć <jakublokiec@wp.pl>
2019-10-02 12:49:13 +0200

    Add league category reducer to root reducer

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { login, LoginState } from './loginReducer';
 import { leagues, LeaguesState } from './leaguesReducer';
+import { categories, CategoriesState } from './categoriesReducer';
 
 export interface CombinedState {
     login: LoginState,
-    leagues: LeaguesState
+    leagues: LeaguesState,
+    categories: CategoriesState
 }
 
-export const rootReducer = combineReducers<CombinedState>({ login, leagues });
+export const rootReducer = combineReducers<CombinedState>({ login, leagues, categories });
