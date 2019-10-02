28b4dde791023b809c1cd80689cc8da7df21c638
Jakub Łokieć <jakublokiec@wp.pl>
2019-10-02 12:48:17 +0200

    Create actions and action creators for league categories

---
+++ src/actions/categories.ts
@@ -0,0 +1,59 @@
+import { ThunkAction, ThunkDispatch } from 'redux-thunk';
+import axios from 'axios';
+import * as api from '../constants/Api';
+import { LeagueCategory } from '../models/LeagueCategory';
+
+export const FETCH_CATEGORIES_START = 'FETCH_CATEGORIES_START';
+export const FETCH_CATEGORIES_SUCCESS = 'FETCH_CATEGORIES_SUCCESS';
+export const FETCH_CATEGORIES_ERROR = 'FETCH_CATEGORIES_ERROR';
+
+export interface FetchCategoriesStartAction {
+    type: string
+}
+
+export interface FetchCategoriesSuccessAction {
+    type: string,
+    categories: Array<LeagueCategory>
+}
+
+export interface FetchCategoriesErrorAction {
+    type: string,
+    error: object
+}
+
+export type CategoriesAction = FetchCategoriesStartAction | FetchCategoriesSuccessAction | FetchCategoriesErrorAction;
+
+export const fetchCategoriesStart = (): FetchCategoriesStartAction => {
+    return {
+        type: FETCH_CATEGORIES_START
+    };
+}
+
+export const fetchCategoriesError = (error: object): FetchCategoriesErrorAction => {
+    return {
+        type: FETCH_CATEGORIES_ERROR,
+        error: error
+    };
+}
+
+export const fetchCategoriesSuccess = (categories: Array<LeagueCategory>): FetchCategoriesSuccessAction => {
+    return {
+        type: FETCH_CATEGORIES_SUCCESS,
+        categories: categories
+    };
+}
+
+export const fetch = (): ThunkAction<Promise<void>, {}, {}, CategoriesAction> => {
+    return async (dispatch: ThunkDispatch<{}, {}, CategoriesAction>): Promise<void> => {
+        dispatch(fetchCategoriesStart());
+        axios.get(api.LEAGUE_CATEGORIES)
+            .then(response => {
+                dispatch(fetchCategoriesSuccess(response.data));
+                Promise.resolve();
+            })
+            .catch(error => {
+                dispatch(fetchCategoriesError(error));
+                Promise.reject();
+            })
+    };
+}
