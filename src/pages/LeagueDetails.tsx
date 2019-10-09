19625d9e6bfae9b5469adb45447c6c4b8f8da4ee
Jakub Łokieć <jakublokiec@wp.pl>
2019-10-09 18:31:06 +0200

    Create league details page

---
+++ src/pages/LeagueDetails.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { RouteComponentProps } from 'react-router-dom';
+import { HOME_NAME } from './Home';
+import { CATEGORIES_NAME } from './Categories';
+import * as routing from '../constants/routing';
+
+interface LeagueId {
+    id: string
+}
+
+interface LeagueDetailsProperties extends RouteComponentProps<LeagueId> {
+
+}
+
+export const LeagueDetails: React.FC<LeagueDetailsProperties> = (props: LeagueDetailsProperties) => {
+    const id = parseInt(props.match.params.id);
+    return (
+        <div>
+            <Breadcrumb>
+                <Breadcrumb.Item href={routing.HOME_ROUTE}>{HOME_NAME}</Breadcrumb.Item>
+                <Breadcrumb.Item href={routing.CATEGORIES_ROUTE}>{CATEGORIES_NAME}</Breadcrumb.Item>
+                <Breadcrumb.Item active>{LEAGUE_DETAILS_NAME}</Breadcrumb.Item>
+            </Breadcrumb>
+            <p>Szczegóły ligi {id}</p>
+        </div>
+    );
+}
+
+export const LEAGUE_DETAILS_NAME = "Szczegóły ligi";
