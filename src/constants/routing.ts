fedee1fc7aa5f59eaca6e960c187a6d477ece6be
Jakub Łokieć <jakublokiec@wp.pl>
2019-10-09 18:00:08 +0200

    Add routing path for league details page

--- src/constants/routing.ts
+++ src/constants/routing.ts
@@ -4,3 +4,5 @@ export const LEAGUES_PATH = "/leagues";
 export const LEAGUES_ROUTE = LEAGUES_PATH + "/:id";
 export const REGISTER_ROUTE = "/register";
 export const LOGIN_ROUTE = "/login";
+export const LEAGUE_DETAILS_PATH = "/league_details";
+export const LEAGUE_DETAILS_ROUTE = LEAGUE_DETAILS_PATH + "/:id";
