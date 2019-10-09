dc87966d3aaa3f0415b048fbe3588268f1dede84
Jakub Łokieć <jakublokiec@wp.pl>
2019-10-09 17:43:26 +0200

    Add path for matches endpoint

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,7 @@
-export const BASE_URL = "http://localhost:8080/api";
-export const LEAGUE_CATEGORIES = "/public/categories";
-export const LEAGUES = "/public/leagues";
-export const REGISTER = "/public/register";
-export const LOGIN = "/auth";
+export const BASE_URL = "http://localhost:8080/api/";
+const PUBLIC_PREFIX = "public/";
+export const LEAGUE_CATEGORIES = PUBLIC_PREFIX + "categories";
+export const LEAGUES = PUBLIC_PREFIX + "leagues";
+export const REGISTER = PUBLIC_PREFIX + "register";
+export const LOGIN = "auth";
+export const MATCHES = PUBLIC_PREFIX + "matches";
