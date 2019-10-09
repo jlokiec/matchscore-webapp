76aa078ca537d2acd3d2f2fa7a5143b1abf1224c
Jakub Łokieć <jakublokiec@wp.pl>
2019-10-09 18:45:01 +0200

    Create reducer for matches

---
+++ src/reducers/matchReducer.ts
@@ -0,0 +1,64 @@
+import {
+    FETCH_MATCHES_START,
+    FETCH_MATCHES_SUCCESS,
+    FETCH_MATCHES_ERROR,
+    MatchesAction,
+    FetchMatchesErrorAction,
+    FetchMatchesSuccessAction,
+} from '../actions/matches';
+import { Match } from '../models/Match';
+
+export interface MatchesState {
+    loading: boolean,
+    error?: object,
+    data: Array<Match>
+}
+
+const initialState = {
+    loading: false,
+    error: undefined,
+    data: []
+}
+
+export const matches = (state: MatchesState = initialState, action: MatchesAction): MatchesState => {
+    switch (action.type) {
+        case FETCH_MATCHES_START:
+            return {
+                ...state,
+                loading: true,
+                error: undefined
+            };
+        case FETCH_MATCHES_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: undefined,
+                data: mergeMatchArrays(state.data, (action as FetchMatchesSuccessAction).matches)
+            };
+        case FETCH_MATCHES_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: (action as FetchMatchesErrorAction).error
+            };
+        default:
+            return state;
+    }
+}
+
+const mergeMatchArrays = (oldState: Array<Match>, fetchedLeagues: Array<Match>) => {
+    return [...oldState, ...fetchedLeagues].reduce((res: Array<Match>, data: Match, index: number, arr: Array<Match>) => {
+        if (res.findIndex(league => league.id === data.id) < 0) {
+            res.push(data);
+        }
+        return res;
+    }, []);
+}
+
+export const getMatchesForLeagueId = (state: MatchesState, leagueId: number) => {
+    return state.data.filter(match => match.homeTeam.leagueId === leagueId && match.awayTeam.leagueId === leagueId);
+}
+
+export const getMatchesForLeagueIdAndRound = (state: MatchesState, leagueId: number, round: number) => {
+    return getMatchesForLeagueId(state, leagueId).filter(match => match.round === round);
+}
