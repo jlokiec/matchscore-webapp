bb38ee1a1372ab228f8039e37f56fbb9fc493e18
Jakub Łokieć <jakublokiec@wp.pl>
2019-12-13 15:16:30 +0100

    Secure app routes regarding user roles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { AppRouting } from './components/Routing';
+import AppRouting from './components/Routing';
 import NavbarMenu from './components/NavbarMenu';
 
 export default class App extends React.Component {
